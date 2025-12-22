bce7fb7a09179e0a70452344ce12f4031e981d6f
sinam128 <Sinasinkogo@gmail.com>
2025-12-22 20:34:11 +0330

    Drawing the simple map

--- main.c
+++ main.c
@@ -1,14 +1,50 @@
 #include "raylib.h"
-
+#define dimension 40
+#define strong 16
 int main() {
-  InitWindow(800, 600, "Hello Raylib");
+  InitWindow(640, 640, "WorldMap");
+
+  SetTargetFPS(60);
+
+  int world_map[strong][strong] = {
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+      {1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1},
+      {1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1},
+      {1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1},
+      {1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1},
+      {1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+      {1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1},
+      {1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+      {1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1},
+      {1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1},
+      {1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1},
+      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+
+  };
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("Hello, Raylib!", 190, 200, 20, RED);
+    for (int i = 0; i < strong; i++) {
+      for (int j = 0; j < strong; j++) {
+        if (world_map[j][i] == 1) {
+          DrawRectangle(i * dimension, j * dimension, dimension, dimension,
+                        DARKGRAY);
+        } else {
+          DrawRectangle(i * dimension, j * dimension, dimension, dimension,
+                        LIGHTGRAY);
+        }
+        DrawRectangleLines(i * dimension, j * dimension, dimension, dimension,
+                           DARKGREEN);
+      }
+    }
     EndDrawing();
   }
   CloseWindow();
+
   return 0;
 }
 // phase1 commit
