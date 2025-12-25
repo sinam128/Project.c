fe63b5d2bce61f1fd087a2f838482456b120c7fe
sinam128 <Sinasinkogo@gmail.com>
2025-12-25 18:25:32 +0330

    hitting the wall and sliding part (final of phase 1)

--- main.c
+++ main.c
@@ -3,6 +3,15 @@
 
 #define dimension 40
 #define strong 16
+#define radius 0.25
+
+float check(float rad) {
+  if (rad > 0) {
+    return radius;
+  } else {
+    return (-1) * radius;
+  }
+}
 int main() {
   int world_map[strong][strong] = {
       {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -35,9 +44,11 @@ int main() {
   gamer.pos.y = 1.5;
   gamer.dir.x = 1.0;
   gamer.dir.y = 0.0;
+  gamer.plane.x = 0.0;
+  gamer.plane.y = -1.0;
   speed.x = 3.5f;
   speed.y = 3.5f;
-  dspeed = 1.5f;
+  dspeed = 1.8f;
   float angle = 0.0f;
   InitWindow(640, 640, "WorldMap");
 
@@ -50,28 +61,70 @@ int main() {
       angle += dt * dspeed;
       gamer.dir.x = cos(angle);
       gamer.dir.y = sin(angle);
+      gamer.plane.x = sin(angle);
+      gamer.plane.y = -cos(angle);
     }
     if (IsKeyDown(KEY_LEFT)) {
       angle -= dt * dspeed;
       gamer.dir.x = cos(angle);
       gamer.dir.y = sin(angle);
+      gamer.plane.x = sin(angle);
+      gamer.plane.y = -cos(angle);
     }
     if (IsKeyDown(KEY_W)) {
-      gamer.pos.y += speed.y * dt * gamer.dir.y;
-      gamer.pos.x += speed.x * dt * gamer.dir.x;
+      if (IsKeyDown(KEY_W)) {
+
+        if (world_map[(int)(gamer.pos.y + (speed.y * dt * gamer.dir.y) +
+                            check(speed.y * dt * gamer.dir.y))]
+                     [(int)gamer.pos.x] != 1) {
+          gamer.pos.y += speed.y * dt * gamer.dir.y;
+        }
+
+        if (world_map[(int)gamer.pos.y]
+                     [(int)(gamer.pos.x + (speed.x * dt * gamer.dir.x) +
+                            check(speed.x * dt * gamer.dir.x))] != 1) {
+          gamer.pos.x += speed.x * dt * gamer.dir.x;
+        }
+      }
     }
     if (IsKeyDown(KEY_S)) {
-      gamer.pos.y -= speed.y * dt * gamer.dir.y;
-      gamer.pos.x -= speed.x * dt * gamer.dir.x;
+      if (world_map[(int)(gamer.pos.y - (speed.y * dt * gamer.dir.y) +
+                          check(-speed.y * dt * gamer.dir.y))]
+                   [(int)gamer.pos.x] != 1) {
+        gamer.pos.y -= speed.y * dt * gamer.dir.y;
+      }
+      if (world_map[(int)gamer.pos.y]
+                   [(int)(gamer.pos.x - (speed.x * dt * gamer.dir.x) +
+                          check(-speed.x * dt * gamer.dir.x))] != 1) {
+        gamer.pos.x -= speed.x * dt * gamer.dir.x;
+      }
     }
     if (IsKeyDown(KEY_A)) {
-      gamer.pos.y -= speed.y * dt * gamer.dir.x;
-      gamer.pos.x += speed.x * dt * gamer.dir.y;
+      if (world_map[(int)(gamer.pos.y - (speed.y * dt * gamer.dir.x) +
+                          check(-speed.y * dt * gamer.dir.x))]
+                   [(int)gamer.pos.x] != 1) {
+        gamer.pos.y -= speed.y * dt * gamer.dir.x;
+      }
+      if (world_map[(int)gamer.pos.y]
+                   [(int)(gamer.pos.x + (speed.x * dt * gamer.dir.y) +
+                          check(speed.x * dt * gamer.dir.y))] != 1) {
+        gamer.pos.x += speed.x * dt * gamer.dir.y;
+      }
     }
+
     if (IsKeyDown(KEY_D)) {
-      gamer.pos.y += speed.y * dt * gamer.dir.x;
-      gamer.pos.x -= speed.x * dt * gamer.dir.y;
+      if (world_map[(int)(gamer.pos.y + (speed.y * dt * gamer.dir.x) +
+                          check(speed.y * dt * gamer.dir.x))]
+                   [(int)gamer.pos.x] != 1) {
+        gamer.pos.y += speed.y * dt * gamer.dir.x;
+      }
+      if (world_map[(int)gamer.pos.y]
+                   [(int)(gamer.pos.x - (speed.x * dt * gamer.dir.y) +
+                          check(-speed.x * dt * gamer.dir.y))] != 1) {
+        gamer.pos.x -= speed.x * dt * gamer.dir.y;
+      }
     }
+
     for (int i = 0; i < strong; i++) {
       for (int j = 0; j < strong; j++) {
         if (world_map[i][j] == 1) {
@@ -89,6 +142,9 @@ int main() {
     DrawLine(gamer.pos.x * dimension, gamer.pos.y * dimension,
              (gamer.pos.x + gamer.dir.x) * dimension,
              (gamer.pos.y + gamer.dir.y) * dimension, DARKBLUE);
+    DrawLine(gamer.pos.x * dimension, gamer.pos.y * dimension,
+             (gamer.pos.x + gamer.plane.x * 0.9) * dimension,
+             (gamer.pos.y + gamer.plane.y * 0.9) * dimension, PINK);
     EndDrawing();
   }
   CloseWindow();
