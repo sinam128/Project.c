2e5efc62119bb3db080703275308db02caa63a20
sinam128 <Sinasinkogo@gmail.com>
2026-01-02 13:32:12 +0330

    World drawing

--- main.c
+++ main.c
@@ -45,10 +45,11 @@ int main() {
   gamer.dir.x = 1.0;
   gamer.dir.y = 0.0;
   gamer.plane.x = 0.0;
-  gamer.plane.y = -1.0;
+  gamer.plane.y = 1.0;
   speed.x = 3.5f;
   speed.y = 3.5f;
   dspeed = 1.8f;
+  int mapflag = 1;
   float angle = 0.0f;
   InitWindow(640, 640, "WorldMap");
 
@@ -57,19 +58,22 @@ int main() {
     float dt = GetFrameTime();
     BeginDrawing();
     ClearBackground(RAYWHITE);
+    if (IsKeyDown(KEY_M)) {
+      mapflag *= (-1);
+    }
     if (IsKeyDown(KEY_RIGHT)) {
       angle += dt * dspeed;
       gamer.dir.x = cos(angle);
       gamer.dir.y = sin(angle);
-      gamer.plane.x = sin(angle);
-      gamer.plane.y = -cos(angle);
+      gamer.plane.x = -sin(angle);
+      gamer.plane.y = cos(angle);
     }
     if (IsKeyDown(KEY_LEFT)) {
       angle -= dt * dspeed;
       gamer.dir.x = cos(angle);
       gamer.dir.y = sin(angle);
-      gamer.plane.x = sin(angle);
-      gamer.plane.y = -cos(angle);
+      gamer.plane.x = -sin(angle);
+      gamer.plane.y = cos(angle);
     }
     if (IsKeyDown(KEY_W)) {
 
@@ -166,7 +170,11 @@ int main() {
       int side;
       int flag = 0;
 
-      while (flag == 0) {
+      int maxSteps = 100;
+      int steps = 0;
+
+      while (flag == 0 && maxSteps > steps) {
+        steps++;
         if (sideDistX < sideDistY) {
           sideDistX += deltaDistX;
           mapX += stepX;
@@ -181,6 +189,7 @@ int main() {
           flag = 1;
         }
       }
+
       double perpWallDist;
 
       if (side == 0)
@@ -202,34 +211,64 @@ int main() {
         color = MAROON;
       }
 
-      DrawLine(i, 0, i, drawStart - 1, SKYBLUE);
+      if (mapflag == 1) {
+        DrawLine(i, 0, i, drawStart - 1, SKYBLUE);
 
-      DrawLine(i, drawStart, i, drawEnd, color);
+        DrawLine(i, drawStart, i, drawEnd, color);
 
-      DrawLine(i, drawEnd + 1, i, 639, GREEN);
-    }
-    for (int i = 0; i < strong; i++) {
-      for (int j = 0; j < strong; j++) {
-        if (world_map[i][j] == 1) {
-          DrawRectangle(j * dimension, i * dimension, dimension, dimension,
-                        DARKGRAY);
-        } else {
-          DrawRectangle(j * dimension, i * dimension, dimension, dimension,
-                        LIGHTGRAY);
-        }
-        DrawRectangleLines(j * dimension, i * dimension, dimension, dimension,
-                           BLACK);
+        DrawLine(i, drawEnd + 1, i, 639, GREEN);
       }
     }
-    DrawCircle(gamer.pos.x * dimension, gamer.pos.y * dimension, 10, RED);
-    DrawLine(gamer.pos.x * dimension, gamer.pos.y * dimension,
-             (gamer.pos.x + gamer.dir.x) * dimension,
-             (gamer.pos.y + gamer.dir.y) * dimension, DARKBLUE);
-    DrawLine(gamer.pos.x * dimension, gamer.pos.y * dimension,
-             (gamer.pos.x + gamer.plane.x * 0.9) * dimension,
-             (gamer.pos.y + gamer.plane.y * 0.9) * dimension, PINK);
+    if (mapflag == 1) {
+      for (int i = 0; i < strong; i++) {
+        for (int j = 0; j < strong; j++) {
+          if (world_map[i][j] == 1) {
+
+            DrawRectangle(640 - strong * 10 + j * 10, 10 + i * 10, 10, 10,
+                          DARKGRAY);
+          } else {
+            DrawRectangle(640 - strong * 10 + j * 10, 10 + i * 10, 10, 10,
+                          LIGHTGRAY);
+          }
+          DrawRectangleLines(640 - strong * 10 + j * 10, 10 + i * 10, 10, 10,
+                             BLACK);
+        }
+      }
+      DrawCircle(640 - strong * 10 + gamer.pos.x * 10, 10 + gamer.pos.y * 10, 3,
+                 RED);
+
+      DrawLine(640 - strong * 10 + gamer.pos.x * 10, 10 + gamer.pos.y * 10,
+               640 - strong * 10 + (gamer.pos.x + gamer.dir.x * 0.5) * 10,
+               10 + (gamer.pos.y + gamer.dir.y * 0.5) * 10, DARKBLUE);
+    } else {
+      for (int i = 0; i < strong; i++) {
+        for (int j = 0; j < strong; j++) {
+          if (world_map[i][j] == 1) {
+            DrawRectangle(j * dimension, i * dimension, dimension, dimension,
+                          DARKGRAY);
+          } else {
+            DrawRectangle(j * dimension, i * dimension, dimension, dimension,
+                          LIGHTGRAY);
+          }
+          DrawRectangleLines(j * dimension, i * dimension, dimension, dimension,
+                             BLACK);
+        }
+      }
+      DrawCircle(gamer.pos.x * dimension, gamer.pos.y * dimension, 10, RED);
+      DrawLine(gamer.pos.x * dimension, gamer.pos.y * dimension,
+               (gamer.pos.x + gamer.dir.x) * dimension,
+               (gamer.pos.y + gamer.dir.y) * dimension, DARKBLUE);
+      DrawLine(gamer.pos.x * dimension, gamer.pos.y * dimension,
+               (gamer.pos.x + gamer.plane.x * 0.9) * dimension,
+               (gamer.pos.y + gamer.plane.y * 0.9) * dimension, PINK);
+    }
+
+    // DrawLine(640 - strong * 10 + gamer.pos.x * 10, 10 + gamer.pos.y * 10,
+    //          640 - strong * 10 + (gamer.pos.x + gamer.plane.x * 0.45) * 10,
+    //          10 + (gamer.pos.y + gamer.plane.y * 0.45) * 10, PINK);
     EndDrawing();
   }
+
   CloseWindow();
 
   return 0;
